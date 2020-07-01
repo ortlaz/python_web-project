a77bd348127ca5ea7f9fcf7a9adeb59aaa6a13cb
ortlaz <anastasea.com@gmail.com>
2020-07-01 22:33:44 +0300

    Файл загрузки nginx

---
+++ init.sh
@@ -0,0 +1,8 @@
+sudo rm /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/ngix/sites-enabled/test.conf
+sudo /etc/init.d/nginx restart
+sudo ln -sf /home/box/web/etc/gunicorn-wsgi.conf /etc/gunicorn.d/test-wsgi
+sudo ln -sf /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/test-django
+sudo /etc/init.d/gunicorn restart
+
+
