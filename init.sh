55fd88066ed262734b8f4089080d1f9a6c668c82
ortlaz <anastasea.com@gmail.com>
2020-07-29 19:32:55 +0300

    Fix bugs for test4

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 sudo rm /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
-sudo /etc/init.d/nginx restart
+sudo nginx -c /etc/nginx/sites-enabled/test.conf restart
 sudo ln -sf /home/box/web/etc/gunicorn-wsgi.conf /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
 gunicorn -c /home/box/web/etc/gunicorn-wsgi.conf home:application
