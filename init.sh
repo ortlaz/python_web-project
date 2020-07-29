293c6126a6cab0c5a50bd11993c344a239b28e46
ortlaz <anastasea.com@gmail.com>
2020-07-29 20:06:22 +0300

    Fix link

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
-sudo rm /etc/nginx/sites-enabled/default
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
-sudo nginx -c /etc/nginx/sites-enabled/test.conf restart
+sudo rm /etc/nginx/sites-available/default
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-availble/test.conf
+sudo /etc/init.d/nginx  restart
 sudo ln -sf /home/box/web/etc/gunicorn-wsgi.conf /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
 gunicorn -c /home/box/web/etc/gunicorn-wsgi.conf home:application
